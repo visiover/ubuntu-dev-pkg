46a3b4538aa76c1f06f3f13b152ce6f741e641f3
Maher M'Hiri <mamh@fynskemedier.dk>
2017-01-25 19:10:37 +0100

    Delete host name from /etc/hosts

--- nginx_delete_site.sh
+++ nginx_delete_site.sh
@@ -4,6 +4,7 @@ sudo rm -rf /etc/nginx/sites-enabled/$1.conf
 sudo rm -rf /etc/nginx/sites-available/$1.conf
 
 #delete the line from the /etc/hosts
+sed -i '/$1/d' /etc/hosts
 
 #restart nginx
 sudo /etc/init.d/nginx restart
