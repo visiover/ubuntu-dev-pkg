45fd7c30097b775e2bb211bf244a5a3344c9bed1
Maher M'Hiri <mamh@fynskemedier.dk>
2017-01-26 11:23:11 +0100

    bugfix special character ignore

--- nginx_add_site.sh
+++ nginx_add_site.sh
@@ -39,7 +39,7 @@ server {
         #fastcgi_pass   127.0.0.1:9100;
         fastcgi_pass unix:/var/run/php/php7.0-fpm.sock;
         fastcgi_index  index.php;
-        fastcgi_param  SCRIPT_FILENAME  \$document_root$fastcgi_script_name;
+        fastcgi_param  SCRIPT_FILENAME  \$document_root\$fastcgi_script_name;
         include fastcgi_params;
     }
 }
