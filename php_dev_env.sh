0ca566bbbb036d5da2a81d8c2f663a87e9f050c2
Maher M'Hiri <mamh@fynskemedier.dk>
2017-01-30 14:39:08 +0100

    Commented out browser-sync

--- php_dev_env.sh
+++ php_dev_env.sh
@@ -97,7 +97,7 @@ sudo apt-get install python-software-properties
 curl -sL https://deb.nodesource.com/setup_7.x | sudo -E bash -
 sudo apt-get install nodejs
 
-echo "Installing browser-sync"
+#echo "Installing browser-sync"
 #sudo npm install -g browser-sync
 
 
