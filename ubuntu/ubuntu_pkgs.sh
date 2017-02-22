067b4e7ff0ecf283d3aa44b225d03af1125f9ab1
Maher M'Hiri <mamh@fynskemedier.dk>
2017-02-22 09:42:16 +0100

    Defaulted user input

--- ubuntu/ubuntu_pkgs.sh
+++ ubuntu/ubuntu_pkgs.sh
@@ -8,42 +8,37 @@ sleep 1  # Waits 2 seconds.
 echo "\n\n***********************************************"
 echo "\t Updating to latest package lists"
 echo "***********************************************"
-sudo apt-get update -y # To get the latest package lists
+sudo apt-get update -y # get the latest package lists
 
-
-sudo apt-get install vim
-sudo apt-get install gnome-do
-sudo apt-get install virtualbox
-sudo apt-get install htop
-
-
-
-#ubuntu cleaner
-sudo add-apt-repository ppa:gerardpuig/ppa
-sudo add-apt-repository ppa:otto-kesselgulasch/gimp
-sudo add-apt-repository ppa:webupd8team/sublime-text-3
-sudo add-apt-repository ppa:fossfreedom/indicator-sysmonitor
+sudo add-apt-repository ppa:gerardpuig/ppa -y # ubuntu cleaner
+sudo add-apt-repository ppa:otto-kesselgulasch/gimp -y
+sudo add-apt-repository ppa:webupd8team/sublime-text-3 -y
+sudo add-apt-repository ppa:fossfreedom/indicator-sysmonitor -y
 sudo apt-get update
 
-sudo apt-get install ubuntu-cleaner
-sudo apt-get install gimp
-sudo apt-get install vlc
-sudo apt-get install filezilla
-sudo apt-get install gtk-recordmydesktop
-sudo apt-get install redshift redshift-gtk
-sudo apt-get install terminator
-sudo apt-get install sublime-text-installer
-sudo apt-get install indicator-sysmonitor
-sudo apt-get install synaptic
-sudo apt-get install gnome-themes-standard
-sudo apt-get install network-manager-openconnect network-manager-openconnect-gnome
+sudo apt-get --assume-yes install vim
+sudo apt-get --assume-yes install gnome-do
+sudo apt-get --assume-yes install virtualbox
+sudo apt-get --assume-yes install htop
+sudo apt-get --assume-yes install ubuntu-cleaner
+sudo apt-get --assume-yes install gimp
+sudo apt-get --assume-yes install vlc
+sudo apt-get --assume-yes install filezilla
+sudo apt-get --assume-yes install gtk-recordmydesktop
+sudo apt-get --assume-yes install redshift redshift-gtk
+sudo apt-get --assume-yes install terminator
+sudo apt-get --assume-yes install sublime-text-installer
+sudo apt-get --assume-yes install indicator-sysmonitor
+sudo apt-get --assume-yes install synaptic
+sudo apt-get --assume-yes install gnome-themes-standard
+sudo apt-get --assume-yes install network-manager-openconnect network-manager-openconnect-gnome
 
 
 #spotify
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys BBEBDCB318AD50EC6865090613B00F1FD2C19886
 echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
 sudo apt-get update
-sudo apt-get install spotify-client
+sudo apt-get --assume-yes install spotify-client
 
 
 check()
